612abf733bc68e2815e70c81d29231d211e06e89
K Mallikarjuna Reddy <115869610+malli13193@users.noreply.github.com>
2024-01-20 23:51:22 +0530

    Add files via upload

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "mallikarjunareddykanala2003@gmail.com"; //sender
+    $mail_sender_password = "jkcz uygf rpde gsev"; //sender
+    $mail_sender_name = "KMS";
+?>
